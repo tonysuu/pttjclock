d045b058065325693392e3106bd5566acc66d116
tonysuu <tonysu1122@hotmail.com>
2014-11-01 21:14:23 -0400

    gfgsdfg

--- app/src/main/java/com/example/tony/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/tony/alarmclock/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -20,6 +22,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void buttonAction(View view)
+    {
+        setContentView(R.layout.layout);
     }
 
     @Override
